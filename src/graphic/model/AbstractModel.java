c2ebb1849dddf1f729bebe50159e9b0ad14a7200
jogar84 <chezmoi@chezmoi-VirtualBox>
2016-10-23 15:41:31 -0400

    commit restant

--- src/graphic/model/AbstractModel.java
+++ src/graphic/model/AbstractModel.java
@@ -5,13 +5,12 @@ import java.util.Observer;
 
 import graphic.observer.Observable;
 
-public abstract class AbstractModel implements Observable {
+public abstract class AbstractModel implements Observable { // UNUSED
 	private ArrayList<Observer> listObserver = new ArrayList<Observer>();
 	
 	public void addObserver(java.util.Observer obs) {
 		this.listObserver.add(obs);
 	}
-
 	public ArrayList<String> getFileList;
 	
 	
--- src/graphic/view/AbstractView.java
+++ src/graphic/view/AbstractView.java
@@ -1,22 +1,46 @@
 package graphic.view;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import javax.swing.JPanel;
 
-import graphic.model.AbstractModel;
+import graphic.controler.ViewerControler;
+import graphic.model.PictureListModel;
+
 
 public abstract class AbstractView extends JPanel {
-
-	private final AbstractModel model;
-	private ArrayList<String> fileList = new ArrayList<>();
 	
-	public AbstractView(AbstractModel model) {
-		this.model = model;
+	private ViewerControler vc;
+	
+	public AbstractView() {
+		vc = null;
 	}
 	
-	public ArrayList<String> getFileList(){
+/*	public ArrayList<String> getFileList(){ // TODO à traiter
 		return fileList;
+	}*/
+	public abstract int getSelectedItemIndex();// TODO check that.
+	
+	public PictureListModel getModel(){
+		return vc.getModel();
+	}
+	
+	public String getCurrentPicture(){
+		return vc.getCurrentPicture();
+	}
+	
+	public int getCurrentIndex(){
+		return vc.getCurrentIndex();
+	}
+	
+	public void setViewerController(ViewerControler vc){
+		Objects.requireNonNull(vc);
+		this.vc = vc;
+	}
+	
+	public void displaySelectedItem(String path){
+		vc.changeCurrentImg(path);
 	}
 	
 	
